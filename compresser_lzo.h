673c3c32bb8dc679ff53caab8bdfa7d7078af7bc
folkert van heusden <mail@vanheusden.com>
2022-01-23 22:09:23 +0100

    compresser: declaration fixes

--- compresser_lzo.h
+++ compresser_lzo.h
@@ -12,6 +12,6 @@ public:
 	compresser_lzo(const int compression_level);
 	virtual ~compresser_lzo();
 
-	virtual bool compress(const uint8_t *const in, const size_t in_len, uint8_t **const out, size_t *const out_len) = 0;
-	virtual bool decompress(const uint8_t *const in, const size_t in_len, uint8_t **const out, size_t *const out_len) = 0;
+	bool compress(const uint8_t *const in, const size_t in_len, uint8_t **const out, size_t *const out_len) override;
+	bool decompress(const uint8_t *const in, const size_t in_len, uint8_t **const out, size_t *const out_len) override;
 };
