5f802cb7ab3cd94c96319920903dd1160bd50878
folkert van heusden <mail@vanheusden.com>
2022-01-24 13:53:07 +0100

    Moved common functions

--- net.cpp
+++ net.cpp
@@ -5,7 +5,9 @@
 #include <unistd.h>
 #include <sys/socket.h>
 
+#include "io.h"
 #include "logging.h"
+#include "net.h"
 #include "str.h"
 
 
@@ -23,3 +25,73 @@ std::string get_endpoint_name(int fd)
 
 	return myformat("[%s]:%s", host, serv);
 }
+
+void add_uint64(std::vector<uint8_t> & target, const uint64_t v)
+{
+	target.push_back(v >> 56);
+	target.push_back(v >> 48);
+	target.push_back(v >> 40);
+	target.push_back(v >> 32);
+	target.push_back(v >> 24);
+	target.push_back(v >> 16);
+	target.push_back(v >>  8);
+	target.push_back(v);
+}
+
+void add_uint32(std::vector<uint8_t> & target, const uint32_t v)
+{
+	target.push_back(v >> 24);
+	target.push_back(v >> 16);
+	target.push_back(v >>  8);
+	target.push_back(v);
+}
+
+void add_uint16(std::vector<uint8_t> & target, const uint16_t v)
+{
+	target.push_back(v >>  8);
+	target.push_back(v);
+}
+
+void add_uint8(std::vector<uint8_t> & target, const uint8_t v)
+{
+	target.push_back(v);
+}
+
+std::optional<uint64_t> receive_uint64(const int fd)
+{
+	uint8_t buffer[8] { 0 };
+	if (READ(fd, buffer, sizeof buffer) != sizeof buffer)
+		return { };
+	
+	return (uint64_t(buffer[0]) << 56) | (uint64_t(buffer[1]) << 48) | (uint64_t(buffer[2]) << 40) | (uint64_t(buffer[3]) << 32) | (uint64_t(buffer[4]) << 24) | (buffer[5] << 16) | (buffer[6] << 8) | buffer[7];
+}
+
+std::optional<uint32_t> receive_uint32(const int fd)
+{
+	uint8_t buffer[4] { 0 };
+	if (READ(fd, buffer, sizeof buffer) != sizeof buffer)
+		return { };
+	
+	return (buffer[0] << 24) | (buffer[1] << 16) | (buffer[2] << 8) | buffer[3];
+}
+
+std::optional<uint16_t> receive_uint16(const int fd)
+{
+	uint8_t buffer[2] { 0 };
+	if (READ(fd, buffer, sizeof buffer) != sizeof buffer)
+		return { };
+	
+	return (buffer[0] << 8) | buffer[1];
+}
+
+std::optional<std::vector<uint8_t> > receive_n_uint8(const int fd, const size_t n)
+{
+	std::vector<uint8_t> out;
+	out.resize(n);
+
+	if (READ(fd, out.data(), n) != ssize_t(n))
+		return { };
+
+	return out;
+}
+
--- net.h
+++ net.h
@@ -1,4 +1,17 @@
+#include <optional>
+#include <stdint.h>
 #include <string>
+#include <vector>
 
 
 std::string get_endpoint_name(int fd);
+
+void add_uint64(std::vector<uint8_t> & target, const uint64_t v);
+void add_uint32(std::vector<uint8_t> & target, const uint32_t v);
+void add_uint16(std::vector<uint8_t> & target, const uint16_t v);
+void add_uint8(std::vector<uint8_t> & target, const uint8_t v);
+
+std::optional<uint64_t> receive_uint64(const int fd);
+std::optional<uint32_t> receive_uint32(const int fd);
+std::optional<uint16_t> receive_uint16(const int fd);
+std::optional<std::vector<uint8_t> > receive_n_uint8(const int fd, const size_t n);
