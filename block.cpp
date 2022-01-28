e386e096ca716490c2dc22c42749ccdfe01dfd27
folkert van heusden <mail@vanheusden.com>
2022-01-28 09:39:58 +0100

    block-wrapper: disable free

--- block.cpp
+++ block.cpp
@@ -6,12 +6,16 @@
 #include "str.h"
 
 
-block::block(const uint8_t *const data, const size_t len) : data(data), len(len)
+block::block(const uint8_t *const data, const size_t len, const bool do_free) : data(data), len(len), do_free(do_free)
+{
+}
+
+block::block(const uint8_t *const data, const size_t len) : data(data), len(len), do_free(true)
 {
 }
 
 // TODO: get rid of this constructor variant (slow & ugly)
-block::block(const std::vector<uint8_t> & data) : data(reinterpret_cast<uint8_t *>(malloc(data.size()))), len(data.size())
+block::block(const std::vector<uint8_t> & data) : data(reinterpret_cast<uint8_t *>(malloc(data.size()))), len(data.size()), do_free(true)
 {
 	if (!this->data)
 		throw myformat("block: cannot allocate %zu bytes of memory", data.size());
@@ -19,14 +23,15 @@ block::block(const std::vector<uint8_t> & data) : data(reinterpret_cast<uint8_t 
 	memcpy(const_cast<uint8_t *>(this->data), data.data(), data.size());
 }
 
-block::block(const block & other) : data(reinterpret_cast<uint8_t *>(malloc(other.get_size()))), len(other.get_size())
+block::block(const block & other) : data(reinterpret_cast<uint8_t *>(malloc(other.get_size()))), len(other.get_size()), do_free(true)
 {
 	memcpy(const_cast<uint8_t *>(this->data), other.get_data(), other.get_size());
 }
 							    
 block::~block()
 {
-	free(const_cast<uint8_t *>(data));
+	if (do_free)
+		free(const_cast<uint8_t *>(data));
 }
 
 bool block::empty() const
--- block.h
+++ block.h
@@ -6,12 +6,15 @@
 
 // This class is a wrapper around a pointer/size pair.
 // The owner ship of the data it wraps is moved to the block class!
+// (unless the not_free constructor is used)
 class block {
 private:
 	const uint8_t *const data;
-	const size_t len;
+	const size_t         len;
+	const bool           do_free;
 
 public:
+	block(const uint8_t *const data, const size_t len, const bool do_free);
 	block(const uint8_t *const data, const size_t len);
 	block(const std::vector<uint8_t> & data);
 	block(const block & other);
