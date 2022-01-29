6b076ca8cf6c7baf1120b5671fa68d74487ae32c
folkert van heusden <mail@vanheusden.com>
2022-01-29 01:08:53 +0100

    formatting

--- io.cpp
+++ io.cpp
@@ -10,12 +10,10 @@ ssize_t READ(int fd, uint8_t *whereto, size_t len)
 {
 	ssize_t cnt=0;
 
-	while(len > 0)
-	{
+	while(len > 0) {
 		ssize_t rc = read(fd, whereto, len);
 
-		if (rc == -1)
-		{
+		if (rc == -1) {
 			if (errno == EINTR || errno == EAGAIN) {
 				dolog(ll_warning, "READ: %s", strerror(errno));
 				continue;
@@ -25,8 +23,7 @@ ssize_t READ(int fd, uint8_t *whereto, size_t len)
 		}
 		else if (rc == 0)
 			break;
-		else
-		{
+		else {
 			whereto += rc;
 			len -= rc;
 			cnt += rc;
@@ -40,12 +37,10 @@ ssize_t WRITE(int fd, const uint8_t *whereto, size_t len)
 {
 	ssize_t cnt=0;
 
-	while(len > 0)
-	{
+	while(len > 0) {
 		ssize_t rc = write(fd, whereto, len);
 
-		if (rc == -1)
-		{
+		if (rc == -1) {
 			if (errno == EINTR || errno == EAGAIN) {
 				dolog(ll_warning, "WRITE: %s", strerror(errno));
 				continue;
@@ -55,8 +50,7 @@ ssize_t WRITE(int fd, const uint8_t *whereto, size_t len)
 		}
 		else if (rc == 0)
 			return -1;
-		else
-		{
+		else {
 			whereto += rc;
 			len -= rc;
 			cnt += rc;
